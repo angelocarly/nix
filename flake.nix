3d400f84ddf8767cc6553df3f5229548c45bc493
Lauda Carly <lauda@nel.re>
2025-07-21 20:02:35 +0200

    Use flakes

---
+++ flake.nix
@@ -0,0 +1,17 @@
+{
+  description = "Nel's NixOS flake";
+
+  inputs = {
+    # NixOS official package source, using the nixos-25.05 branch here
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
+  };
+
+  outputs = { self, nixpkgs, ... }: {
+    nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
+      system = "x86_64-linux";
+      modules = [
+        ./configuration.nix
+      ];
+    };
+  };
+}
